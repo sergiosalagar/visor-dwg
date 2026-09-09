9f5d8778bca70bc3446c8624b637a07ad58d5c2d
sergiosalagar <sergiosalagar@gmail.com>
2026-09-09 15:42:08 +0200

    v05

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'visor-dwg-v4';
+const CACHE_NAME = 'visor-dwg-v6';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
